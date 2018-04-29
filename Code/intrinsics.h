bd610b765cda1b891ef345c52dc0b96d22dc3b9e
JasonBricco <jasonbricco@gmail.com>
2018-04-28 22:58:54 -0500

    -Added more SIMD intrinsics.

--- Code/intrinsics.h
+++ Code/intrinsics.h
@@ -18,6 +18,35 @@ namespace SIMD
 		return _mm_set_ps(a, b, c, d);
 	}
 
+	// Cast from a float4 to an int4.
+	__forceinline int4 Cast(float4 value)
+	{
+		return _mm_castps_si128(value);
+	}
+
+	// Converts a float4 value to an int4 value.
+	__forceinline int4 Convert(float4 value)
+	{
+		return _mm_cvttps_epi32(value);
+	}
+
+	// Given two float4 values, for example:
+	// R3 R2 R1 R0
+	// G3 G2 G1 G0 
+	// Returns the interleaved value:
+	// G1 R1 G0 R0 
+	__forceinline int4 UnpackLo(int4 a, int4 b)
+	{
+		return _mm_unpacklo_epi32(a, b);
+	}
+
+	// Like UnpackLo(), but returns:
+	// G3 R3 G2 R2
+	__forceinline int4 UnpackHi(int4 a, int4 b)
+	{
+		return _mm_unpackhi_epi32(a, b);
+	}
+
 	__forceinline float4 Add(float4 a, float4 b)
 	{
 		return _mm_add_ps(a, b);
@@ -61,5 +90,25 @@ namespace SIMD
 	__forceinline float4 Clamp(float4 value, float4 min, float4 max)
 	{
 		return _mm_min_ps(_mm_max_ps(value, min), max);
+	}
+
+	__forceinline float4 Or(float4 a, float4 b)
+	{
+		return _mm_or_ps(a, b);
+	}
+
+	__forceinline int4 Or(int4 a, int4 b)
+	{
+		return _mm_or_si128(a, b);
+	}
+
+	__forceinline int4 ShiftLeft(int4 a, int amount)
+	{
+		return _mm_slli_epi32(a, amount);
+	}
+
+	__forceinline int4 WriteUnaligned(int4* loc, int4 value)
+	{
+		_mm_storeu_si128(loc, value);
 	}
 }
